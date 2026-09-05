446f1a9d093124c65180fe6ea51a6c25cebb7d61
LopezSuastegui01 <flopez65@ucol.mx>
2026-09-04 23:37:45 -0600

    Hola

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("================================\n");
+printf("HOLA, GITHUB! \n");
+printf("================================ \n");
+printf("Mi nombre es: Fatima Guadalupe \n");
+printf("Estoy aprendiendo programacion.\n");
+printf("Este es mi primer programa\n");
+printf("publicado en GitHub.\n");
+printf("Bienvenido a mi repositorio!\n");
+    return 0;
+}
